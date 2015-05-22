c03230aaf9ea0c9d415a3102f98c1df557f930e9
Jan Kristian Bjerke <jan@crondale.no>
2015-05-22 10:37:01 +0200

    Add CSOrdNo

--- Crondale.VismaEdi/Model/Order.cs
+++ Crondale.VismaEdi/Model/Order.cs
@@ -10,12 +10,12 @@ namespace Crondale.VismaEdi.Model
     [EdiElement(Name = "OrderHead")]
     public class Order : EdiModel
     {
-
+        
         public String TrTp { get; set; }
         public String OrdTp { get; set; }
         public String OrdDt { get; set; }
         public String CustNo { get; set; }
-
+        public String CSOrdNo { get; set; }
 
         public List<OrderLine> OrderLines { get; private set; }
 
